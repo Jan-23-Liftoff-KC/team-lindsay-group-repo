760911659b4ce02512c66a3c882ab1a286d31d64
Kevin Peery <kevin.peery@gmail.com>
2023-01-08 20:48:38 -0600

    complete project setup

---
+++ src/test/java/org/launchcode/medicalapp/MedicalappApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.medicalapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
