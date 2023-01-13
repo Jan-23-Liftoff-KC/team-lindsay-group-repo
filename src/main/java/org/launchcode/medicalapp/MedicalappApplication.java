fcb4be1a63eb768dcce311e7baf2fb8e8d04fa4c
Kevin Peery <kevin.peery@gmail.com>
2023-01-13 09:40:08 -0600

    Line 26 of build.grade commented out to fix error with the expectation of a database connection

--- src/main/java/org/launchcode/medicalapp/MedicalappApplication.java
+++ src/main/java/org/launchcode/medicalapp/MedicalappApplication.java
@@ -3,11 +3,10 @@ package org.launchcode.medicalapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class MedicalappApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MedicalappApplication.class, args);
 	}
-
 }
