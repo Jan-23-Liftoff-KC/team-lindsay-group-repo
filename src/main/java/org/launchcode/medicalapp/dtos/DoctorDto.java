4939ece95baa66ff43ca00d9e0dd038b8edee076
Kevin Peery <kevin.peery@gmail.com>
2023-01-23 18:45:46 -0600

    clean comments

--- src/main/java/org/launchcode/medicalapp/dtos/DoctorDto.java
+++ src/main/java/org/launchcode/medicalapp/dtos/DoctorDto.java
@@ -18,6 +18,10 @@ public class DoctorDto implements Serializable {
     private String doctorname;
     private String password;
     private Set<DoctorDto> doctorDtoSet = new HashSet<>();
+//we transfer an object of data every time. We simply transfer that object instead of calling everything seperately.
+    //this handles the "new" creation for you. It's a cleaner way to transfer objects/info and it's more secure.
+    //It encrypts the passwords when you are sending those.
+    //dtos help us create the object in a clean manner. The services help us access those objects.
 
     public DoctorDto(Doctor doctor) {
         if (doctor.getId() != null){
--- src/main/java/org/launchcode/medicalapp/dtos/PatientDto.java
+++ src/main/java/org/launchcode/medicalapp/dtos/PatientDto.java
@@ -18,31 +18,6 @@ public class PatientDto implements Serializable {
     private String firstName;
     private String lastName;
     private Integer age;
-
-//    private Date appointment;
-//    private static final FastDateFormat dateFormat = FastDateFormat.getInstance("yyyy-MM-dd'T'HH:mm:ss");
-//    String pattern = "EEEEE MMMMM yyyy HH:mm:ss.SSSZ";
-//    String pattern = "EEE, dd MMM yyyy HH:mm:ss Z";
-
-    //FastDateFormat fdf = FastDateFormat.getInstance(PATTERN, TimeZone.getTimeZone("UTC"));
-    //When you put a date object in the app, you may have to do conversion and set
-    //the fields individually b/c the date objects may not be compatible out of the box.
-    //How to send FastDateFormat into a Date object that compatible with SQL or is there another
-    //date format in Java that can be used instead of FastDateFormat but which covers
-    //Year Month Day and Time (Which is also part of Date object)
-    //How do you bring the Calendar from the frontend?
-    //Can start with date then add time.
-
-    //How to call an API from Java: If bringing in info...may be able to simply call it from
-    //the JS side (frontend). We should not store any of that date. Do a fetch request.
-    //If on the frontend you don't have to worry about API calls and you get back the information.
-    //on front end you need to format and parse the info and display it nicely in a column/row, etc...
-
-    //parsing of the date with UTC timestamp on the JS side to the Java side in the PatientDTO.
-    //If JS generates timestamp then I should be able to use a datetime object which parses for you.
-    //In java if you create dto object and add another field, if the type of that field is the
-    //DateTime object, JSON should parse into the object without problems. Add the new property
-    //to the patientDTO and the property of that property will be DateTime.
     private DoctorDto doctorDto;
 
     public PatientDto(Patient patient){
