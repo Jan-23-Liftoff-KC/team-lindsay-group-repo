921736d00b9db73bdcd9fee138b3858896c38179
Kavin Moreno <kavinmoreno@gmail.com>
2023-02-09 00:47:20 -0600

    Revert "Doctor Notes associated files created"

--- src/main/java/org/launchcode/medicalapp/controllers/HomeController.java
+++ src/main/java/org/launchcode/medicalapp/controllers/HomeController.java
@@ -9,6 +9,5 @@ public class HomeController {
     @GetMapping("index")
     public String home (Model model) {
         return "index";
-        //testing
     }
 }
--- src/test/java/org/launchcode/medicalapp/MedicalappApplicationTests.java
+++ src/test/java/org/launchcode/medicalapp/MedicalappApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MedicalAppApplicationTests {
+class MedicalappApplicationTests {
 
 	@Test
 	void contextLoads() {
