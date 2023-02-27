c04faa296b9366bb3eea385ce4a60103a779f9fb
Meg Hynek <110205760+meghynek@users.noreply.github.com>
2023-02-27 17:13:43 -0600

    remove notes experiment, update css

--- src/main/java/org/launchcode/medicalapp/repositories/NotesRepository.java
+++
@@ -1,8 +0,0 @@
-package org.launchcode.medicalapp.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NotesRepository extends JpaRepository {
-}
